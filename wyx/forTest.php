5f4c494cb891b727808d1d9a2fcbf848901afb54
ggWithZhou <wyx0525@mail.ustc.edu.cn>
2016-04-29 10:53:29 +0800

    upload 7 test files

---
+++ wyx/forTest.php
@@ -0,0 +1,9 @@
+<?php
+/*$con = mysql_connect("localhost", "root", "Besafe");
+if (!$con)
+ {
+ die('Could not connect: ' . mysql_error());
+ }
+ echo "Connected successfully."*/
+ echo "Test";
+ ?>
---
+++ wyx/test.js
@@ -0,0 +1,1 @@
+document.getElementById("txtHint").innerHTML="Success";
